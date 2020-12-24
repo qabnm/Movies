4531f4a716d29be73d8a3f21b033f8cb1f9639cc
jun.liu <liujun2@nucarf.com>
2020-12-24 15:58:10 +0800

    Feature:集成友盟统计

--- common/src/main/java/com/jun/liu/common/BaseApplication.kt
+++ common/src/main/java/com/jun/liu/common/BaseApplication.kt
@@ -5,23 +5,39 @@ import android.content.Context
 import com.alibaba.android.arouter.launcher.ARouter
 import com.jun.liu.common.util.ClassUtils
 import com.jun.liu.common.util.Utils
+import com.umeng.commonsdk.UMConfigure
 import dc.android.bridge.view.BaseFragment
 
 open class BaseApplication : Application() {
-    companion object{
-        lateinit var baseCtx:Context
+    companion object {
+        lateinit var baseCtx: Context
     }
+
     override fun onCreate() {
         super.onCreate()
         baseCtx = this.applicationContext
         initARouter()
+        initUmeng()
+    }
+
+    /**
+     * 初始化友盟统计
+     * UMConfigure.init(Context context, String appkey, String channel, int deviceType, String pushSecret);
+     */
+    private fun initUmeng() {
+        UMConfigure.init(applicationContext, "","",0, "")
+        /**
+         * 设置组件化的Log开关
+         * 参数: boolean 默认为false，如需查看LOG设置为true
+         */
+        UMConfigure.setLogEnabled(false)
     }
 
     /**
      * 初始化arouter
      */
-    private fun initARouter(){
-        if (Utils.isAppDebug()){
+    private fun initARouter() {
+        if (Utils.isAppDebug()) {
             ARouter.openLog()
             ARouter.openDebug()
         }
