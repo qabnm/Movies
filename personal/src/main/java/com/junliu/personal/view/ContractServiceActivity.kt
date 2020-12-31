70f9c0455aca1e30a78539c4c5e21fb7d2bf0b8f
jun.liu <jun.liu@163.com>
2020-12-31 11:00:36 +0800

    Feature:联系客服页面

---
+++ personal/src/main/java/com/junliu/personal/view/ContractServiceActivity.kt
@@ -0,0 +1,14 @@
+package com.junliu.personal.view
+
+import com.junliu.personal.R
+import dc.android.bridge.view.BridgeActivity
+
+/**
+ * @author: jun.liu
+ * @date: 2020/12/31 : 10:23
+ * 联系客服
+ *
+ */
+class ContractServiceActivity :BridgeActivity(){
+    override fun getLayoutId() = R.layout.activity_contract_service
+}
