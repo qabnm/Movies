f8634d375b73bdef2d9bfb84034f957953f54947
jun.liu <jun.liu@163.com>
2020-12-31 18:41:34 +0800

    Feature:个人中心页面UI

--- personal/src/main/java/com/junliu/personal/view/ContractServiceActivity.kt
+++ personal/src/main/java/com/junliu/personal/view/ContractServiceActivity.kt
@@ -1,9 +1,12 @@
 package com.junliu.personal.view
 
+import android.text.Editable
+import android.text.TextWatcher
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.junliu.common.util.RouterPath.Companion.PATH_CONTRACT_SERVICE_ACTIVITY
 import com.junliu.personal.R
 import dc.android.bridge.view.BridgeActivity
+import kotlinx.android.synthetic.main.activity_contract_service.*
 
 /**
  * @author: jun.liu
@@ -14,4 +17,17 @@ import dc.android.bridge.view.BridgeActivity
 @Route(path = PATH_CONTRACT_SERVICE_ACTIVITY)
 class ContractServiceActivity : BridgeActivity() {
     override fun getLayoutId() = R.layout.activity_contract_service
+
+    override fun initData() {
+        etSuggest.addTextChangedListener(TextChangeListener())
+    }
+
+    private class TextChangeListener: TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+        override fun afterTextChanged(s: Editable?) {
+        }
+    }
 }
--- personal/src/main/java/com/junliu/personal/view/PersonalFragment.kt
+++ personal/src/main/java/com/junliu/personal/view/PersonalFragment.kt
@@ -2,6 +2,7 @@ package com.junliu.personal.view
 
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
+import com.junliu.common.util.RouterPath.Companion.PATH_CONTRACT_SERVICE_ACTIVITY
 import com.junliu.common.util.RouterPath.Companion.PATH_PERSONAL
 import com.junliu.common.util.RouterPath.Companion.PATH_SETTING_ACTIVITY
 import com.junliu.personal.R
@@ -11,15 +12,23 @@ import kotlinx.android.synthetic.main.fragment_personal.*
 /**
  * @author: jun.liu
  * @date: 2020/12/29 : 14:49
+ * 个人中心
  */
 @Route(path = PATH_PERSONAL)
 class PersonalFragment :BaseFragment() {
     override fun getLayoutId() = R.layout.fragment_personal
 
     override fun initView() {
-        tvMine.setOnClickListener {
-            ARouter.getInstance().build(PATH_SETTING_ACTIVITY).navigation()
-//            startActivity(Intent(requireActivity() , SettingActivity::class.java))
+        layoutHistory.setOnClickListener {  }
+        layoutDownload.setOnClickListener {  }
+        layoutCollection.setOnClickListener {  }
+        layoutShare.setOnClickListener {  }
+        layoutContract.setOnClickListener {
+            ARouter.getInstance().build(PATH_CONTRACT_SERVICE_ACTIVITY).navigation()
         }
+        layoutSetting.setOnClickListener {
+            ARouter.getInstance().build(PATH_SETTING_ACTIVITY).navigation()
+        }
+        layoutAbout.setOnClickListener {  }
     }
 }
