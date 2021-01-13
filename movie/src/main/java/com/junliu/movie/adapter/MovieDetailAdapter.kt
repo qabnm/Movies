5a3c564878f9377a1958e8d6a822d8ceb7b5d621
jun.liu <liujun2@nucarf.com>
2021-01-13 18:25:22 +0800

    Feature:影片详情页面adapter

---
+++ movie/src/main/java/com/junliu/movie/adapter/MovieDetailAdapter.kt
@@ -0,0 +1,27 @@
+package com.junliu.movie.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @author: jun.liu
+ * @date: 2021/1/13 18:24
+ * @des:影片详情
+ */
+class MovieDetailAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return super.getItemViewType(position)
+    }
+}
---
+++ movie/src/main/java/com/junliu/movie/bean/MovieDetailBean.kt
@@ -0,0 +1,9 @@
+package com.junliu.movie.bean
+
+/**
+ * @author: jun.liu
+ * @date: 2021/1/13 18:09
+ * @des:影片详情bean类
+ */
+class MovieDetailBean {
+}
