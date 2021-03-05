5f67a5d8fd41ce12ff81bf6d20e223fe48a4ca59
jun.liu <liujun2@nucarf.com>
2021-03-05 17:41:18 +0800

    Feature:首页tableBar更换

--- main/src/main/java/com/duoduovv/main/view/MainActivity.kt
+++ main/src/main/java/com/duoduovv/main/view/MainActivity.kt
@@ -23,7 +23,7 @@ import kotlin.system.exitProcess
  * 首页activity
  */
 @Route(path = RouterPath.PATH_MAIN)
-class MainActivity : BridgeActivity(), BottomNavBar.OnNavBarClickListener {
+class MainActivity : BridgeActivity() {
     override fun getLayoutId() = R.layout.activity_main
     private var exitTime = 0L
     private var currentPosition = 0
@@ -35,56 +35,53 @@ class MainActivity : BridgeActivity(), BottomNavBar.OnNavBarClickListener {
     private var mineFragment: BaseFragment? = null
 
     override fun initView() {
-        navigation.setNavBarClickListener(this)
+//        navigation.setNavBarClickListener(this)
     }
 
     override fun initData(savedInstanceState: Bundle?) {
-//        navigation.itemIconTintList = null
+        navigation.itemIconTintList = null
         if (savedInstanceState != null) {
             currentPosition = savedInstanceState.getInt(position)
             cinemaFragment = supportFragmentManager.getFragment(
-                savedInstanceState,
-                PATH_CINEMA
+                savedInstanceState, PATH_CINEMA
             ) as? BaseFragment?
             hotSpotFragment = supportFragmentManager.getFragment(
-                savedInstanceState,
-                PATH_HOTSPOT
+                savedInstanceState, PATH_HOTSPOT
             ) as? BaseFragment?
             movieFragment =
                 supportFragmentManager.getFragment(savedInstanceState, PATH_MOVIE) as? BaseFragment?
             mineFragment = supportFragmentManager.getFragment(
-                savedInstanceState,
-                PATH_PERSONAL
+                savedInstanceState, PATH_PERSONAL
             ) as? BaseFragment?
         }
-//        navigation.setOnNavigationItemSelectedListener {
-//            val ts = supportFragmentManager.beginTransaction()
-//            hideAllFragment(ts)
-//            when (it.itemId) {
-//                R.id.action_main -> showFragment(ts, cinemaFragment, PATH_CINEMA, 0)
-//                R.id.action_discover -> showFragment(ts, movieFragment, PATH_MOVIE, 1)
-//                R.id.action_hot -> showFragment(ts, hotSpotFragment, PATH_HOTSPOT, 2)
-//                R.id.action_mine -> showFragment(ts, mineFragment, PATH_PERSONAL, 3)
-//            }
-//            ts.commitAllowingStateLoss()
-//            true
-//        }
-//        navigation.selectedItemId = checkPage(currentPosition)
-        onNavClick(0)
+        navigation.setOnNavigationItemSelectedListener {
+            val ts = supportFragmentManager.beginTransaction()
+            hideAllFragment(ts)
+            when (it.itemId) {
+                R.id.action_main -> showFragment(ts, cinemaFragment, PATH_CINEMA, 0)
+                R.id.action_discover -> showFragment(ts, movieFragment, PATH_MOVIE, 1)
+                R.id.action_hot -> showFragment(ts, hotSpotFragment, PATH_HOTSPOT, 2)
+                R.id.action_mine -> showFragment(ts, mineFragment, PATH_PERSONAL, 3)
+            }
+            ts.commitAllowingStateLoss()
+            true
+        }
+        navigation.selectedItemId = checkPage(currentPosition)
+//        onNavClick(0)
     }
 
-//    /**
-//     * 切换首页tab显示
-//     * @param position Int
-//     * @return Int
-//     */
-//    private fun checkPage(position: Int) = when (position) {
-//        0 -> R.id.action_main
-//        1 -> R.id.action_hot
-//        2 -> R.id.action_discover
-//        3 -> R.id.action_mine
-//        else -> R.id.action_main
-//    }
+    /**
+     * 切换首页tab显示
+     * @param position Int
+     * @return Int
+     */
+    private fun checkPage(position: Int) = when (position) {
+        0 -> R.id.action_main
+        1 -> R.id.action_hot
+        2 -> R.id.action_discover
+        3 -> R.id.action_mine
+        else -> R.id.action_main
+    }
 
     /**
      * 意外退出时，保存当前的fragment
@@ -126,32 +123,32 @@ class MainActivity : BridgeActivity(), BottomNavBar.OnNavBarClickListener {
         }
     }
 
-    override fun onNavClick(position: Int) {
-        val ts = supportFragmentManager.beginTransaction()
-        hideAllFragment(ts)
-        var fragment: BaseFragment? = null
-        var path = ""
-        when (position) {
-            0 -> {
-                fragment = cinemaFragment
-                path = PATH_CINEMA
-            }
-            2 -> {
-                fragment = movieFragment
-                path = PATH_MOVIE
-            }
-            1 -> {
-                fragment = hotSpotFragment
-                path = PATH_HOTSPOT
-            }
-            3 -> {
-                fragment = mineFragment
-                path = PATH_PERSONAL
-            }
-        }
-        showFragment(ts, fragment, path, position)
-        ts.commit()
-    }
+//    override fun onNavClick(position: Int) {
+//        val ts = supportFragmentManager.beginTransaction()
+//        hideAllFragment(ts)
+//        var fragment: BaseFragment? = null
+//        var path = ""
+//        when (position) {
+//            0 -> {
+//                fragment = cinemaFragment
+//                path = PATH_CINEMA
+//            }
+//            2 -> {
+//                fragment = movieFragment
+//                path = PATH_MOVIE
+//            }
+//            1 -> {
+//                fragment = hotSpotFragment
+//                path = PATH_HOTSPOT
+//            }
+//            3 -> {
+//                fragment = mineFragment
+//                path = PATH_PERSONAL
+//            }
+//        }
+//        showFragment(ts, fragment, path, position)
+//        ts.commit()
+//    }
 
     /**
      * 隐藏所有的fragment
