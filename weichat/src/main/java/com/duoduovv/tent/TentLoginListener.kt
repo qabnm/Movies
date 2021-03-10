f3acd5a4de6f11e43b74f2e864645b3d08fa03fe
jun.liu <liujun2@nucarf.com>
2021-03-10 18:28:05 +0800

    Feature:QQ登录调试

--- weichat/src/main/java/com/duoduovv/tent/TentLoginListener.kt
+++ weichat/src/main/java/com/duoduovv/tent/TentLoginListener.kt
@@ -17,11 +17,11 @@ import org.json.JSONObject
  * @date: 2021/2/18 11:43
  * @des:
  */
-class TentLoginListener(private val context: Context):IUiListener {
+class TentLoginListener(private val context: Context) : IUiListener {
     override fun onComplete(response: Any?) {
         val jsonObject = response as? JSONObject
-        Log.i("tent",jsonObject?.toString()?:"jsonObject是空的")
-        if (null != jsonObject){
+        Log.d("tent", jsonObject?.toString() ?: "jsonObject是空的")
+        if (null != jsonObject) {
             val expiresIn = jsonObject.getString(Constants.PARAM_EXPIRES_IN)
             val openId = jsonObject.getString(Constants.PARAM_OPEN_ID)
             val accessToken = jsonObject.getString(Constants.PARAM_ACCESS_TOKEN)
@@ -30,35 +30,35 @@ class TentLoginListener(private val context: Context):IUiListener {
                 it.openId = openId
                 it.setAccessToken(accessToken, expiresIn)
                 val userInfo = UserInfo(context, it.qqToken)
-                val listener = object :IUiListener{
+                val listener = object : IUiListener {
                     override fun onComplete(p0: Any?) {
                         val js = p0 as? JSONObject
                         js?.let {
-                            Log.i("tent",it.toString())
+                            Log.d("tent", it.toString())
                             val ret = it.getInt("ret")
                             val nickName = it.getString("nickname")
                             val sex = it.getInt("gender_type")
                             val msg = it.getString("msg")
                             var headerUrl = it.getString("figureurl_qq_1")
-                            if (it.has("figureurl_qq_2")){
+                            if (it.has("figureurl_qq_2")) {
                                 val url = it.getString("figureurl_qq_2")
                                 if (!TextUtils.isEmpty(url)) headerUrl = url
                             }
-                            val info = TentUserInfo(ret, msg, nickName, sex, headerUrl,openId)
+                            val info = TentUserInfo(ret, msg, nickName, sex, headerUrl, openId)
                             LiveDataBus.get().with("tentUserInfo").value = info
                         }
                     }
 
                     override fun onError(p0: UiError?) {
-                        Log.i("tent","onError${p0?.toString()}")
+                        Log.d("tent", "onError${p0?.toString()}")
                     }
 
                     override fun onCancel() {
-                        Log.i("tent","onCancel")
+                        Log.d("tent", "onCancel")
                     }
 
                     override fun onWarning(p0: Int) {
-                        Log.i("tent","onWarning${p0}")
+                        Log.d("tent", "onWarning${p0}")
                     }
                 }
                 userInfo.getUserInfo(listener)
@@ -78,7 +78,7 @@ class TentLoginListener(private val context: Context):IUiListener {
 //                    }
 //                })
             }
-        }else{
+        } else {
             Toast.makeText(context, "授权失败", Toast.LENGTH_SHORT).show()
         }
     }
@@ -91,6 +91,7 @@ class TentLoginListener(private val context: Context):IUiListener {
     }
 
     override fun onError(error: UiError) {
-        Toast.makeText(context, "授权失败${error.errorCode}:${error.errorMessage}", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "授权失败${error.errorCode}:${error.errorMessage}", Toast.LENGTH_SHORT)
+            .show()
     }
 }
